a3f7b055f145c59e1996791b9b9f5b8bd37a29cf
Christian Tischer <christian.tischer@embl.de>
2021-10-06 17:13:01 +0200

    Fixes #7 Change Ellipsoid Parameter Names

--- src/main/java/ch/epfl/biop/bdv/command/transform/Elliptic3DTransformCreatorCommand.java
+++ src/main/java/ch/epfl/biop/bdv/command/transform/Elliptic3DTransformCreatorCommand.java
@@ -3,21 +3,18 @@ package ch.epfl.biop.bdv.command.transform;
 import bdv.util.Elliptical3DTransform;
 import org.scijava.ItemIO;
 import org.scijava.command.CommandService;
-import org.scijava.object.ObjectService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import sc.fiji.bdvpg.scijava.ScijavaBdvDefaults;
 import sc.fiji.bdvpg.scijava.command.BdvPlaygroundActionCommand;
 
-import java.util.concurrent.ExecutionException;
-
 @Plugin(type = BdvPlaygroundActionCommand.class, menuPath = ScijavaBdvDefaults.RootMenu+"Sources>Transform>New Elliptic 3D Transform")
 public class Elliptic3DTransformCreatorCommand implements BdvPlaygroundActionCommand {
 
     @Parameter
-    double r1, r2, r3, //radius of axes 1 2 3 of ellipse
-            rx, ry, rz, // 3D rotation euler angles  - maybe not the best parametrization
-            tx, ty, tz; // ellipse center
+    double radiusX, radiusY, radiusZ, // radii of axes 1 2 3 of ellipse
+            rotationX, rotationY, rotationZ, // 3D rotation euler angles  - maybe not the best parametrization
+            centerX, centerY, centerZ; // ellipse center
 
     @Parameter(type = ItemIO.OUTPUT)
     private Elliptical3DTransform e3Dt;
@@ -29,15 +26,15 @@ public class Elliptic3DTransformCreatorCommand implements BdvPlaygroundActionCom
     public void run() {
         e3Dt = new Elliptical3DTransform();
         e3Dt.setParameters(
-                "r1", r1,
-                "r2", r2,
-                "r3", r3,
-                "rx", rx,
-                "ry", ry,
-                "rz", rz,
-                "tx", tx,
-                "ty", ty,
-                "tz", tz);
+                "radiusX", radiusX,
+                "radiusY", radiusY,
+                "radiusZ", radiusZ,
+                "rotationX", rotationX,
+                "rotationY", rotationY,
+                "rotationZ", rotationZ,
+                "centerX", centerX,
+                "centerY", centerY,
+                "centerZ", centerZ );
 
         cs.run(DisplayEllipseFromTransformCommand.class, false, "rMin", 0.9, "rMax", 1.1, "e3Dt", e3Dt);
     }
---
+++ src/test/java/DemoImagePlusExport.java
@@ -0,0 +1,65 @@
+import bdv.util.BdvHandle;
+import bdv.viewer.SourceAndConverter;
+import ch.epfl.biop.bdv.command.exporter.BasicBdvViewToImagePlusExportCommand;
+import ch.epfl.biop.bdv.command.register.Elastix2DAffineRegisterCommand;
+import mpicbg.spim.data.generic.AbstractSpimData;
+import net.imagej.ImageJ;
+import net.imagej.patcher.LegacyInjector;
+import org.scijava.command.CommandService;
+import sc.fiji.bdvpg.bdv.navigate.ViewerTransformAdjuster;
+import sc.fiji.bdvpg.services.SourceAndConverterServices;
+import sc.fiji.bdvpg.sourceandconverter.display.BrightnessAutoAdjuster;
+import sc.fiji.bdvpg.spimdata.importer.SpimDataFromXmlImporter;
+
+public class DemoImagePlusExport
+{
+    static final ImageJ ij = new ImageJ();
+
+    static {
+        LegacyInjector.preinit();
+    }
+
+    static public void main(String... args) {
+        ij.ui().showUI();
+
+        demo();
+    }
+
+
+    public static void demo() {
+        final String filePath = "src/test/resources/mri-stack.xml";
+        // Import SpimData
+        SpimDataFromXmlImporter importer = new SpimDataFromXmlImporter(filePath);
+
+        AbstractSpimData spimData = importer.get();
+
+        SourceAndConverter sac = SourceAndConverterServices
+                .getSourceAndConverterService()
+                .getSourceAndConverterFromSpimdata(spimData)
+                .get(0);
+
+        // Creates a BdvHandle
+        BdvHandle bdvHandle = SourceAndConverterServices.getBdvDisplayService().getActiveBdv();
+
+        // Show the sourceandconverter
+        SourceAndConverterServices.getBdvDisplayService().show(bdvHandle, sac);
+        new BrightnessAutoAdjuster(sac, 0).run();
+        new ViewerTransformAdjuster(bdvHandle, sac).run();
+
+        // Export
+        ij.context()
+                .getService( CommandService.class)
+                .run( BasicBdvViewToImagePlusExportCommand.class, true,
+                        "bdv_h", bdvHandle,
+                        "capturename", "image",
+                        "zsize", 20,
+                        "samplingxyinphysicalunit", 1,
+                        "samplingzinphysicalunit", 1,
+                        "interpolate", true,
+                        "unit", "px",
+                        "selected_timepoints_str", "",
+                        "export_mode", "Normal",
+                        "range", ""
+                );
+    }
+}
