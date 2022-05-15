3d942af7995cdc06665546ba8e29467e96a81385
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2022-05-15 18:29:53 +0200

    bumps version of bdv-bioformats

--- src/main/java/ch/epfl/biop/scijava/command/source/SourcesMakeModelCommand.java
+++ src/main/java/ch/epfl/biop/scijava/command/source/SourcesMakeModelCommand.java
@@ -1,6 +1,7 @@
 package ch.epfl.biop.scijava.command.source;
 
 import bdv.viewer.SourceAndConverter;
+import ch.epfl.biop.bdv.bioformats.export.ometiff.OMETiffPyramidizerExporter;
 import ch.epfl.biop.sourceandconverter.SourceHelper;
 import org.scijava.ItemIO;
 import org.scijava.command.Command;
