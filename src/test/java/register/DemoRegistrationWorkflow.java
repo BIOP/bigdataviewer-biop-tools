30a2f9717d33c4312165cfc9174afe978a0a97ca
Nicolas Chiaruttini <nicolas.chiaruttini@epfl.ch>
2024-06-19 16:33:51 +0200

    Limit logging

--- src/test/java/register/DemoRegistrationWorkflow.java
+++ src/test/java/register/DemoRegistrationWorkflow.java
@@ -3,6 +3,7 @@ package register;
 import ch.epfl.biop.bdv.img.qupath.command.CreateBdvDatasetQuPathCommand;
 import ch.epfl.biop.registration.scijava.command.PairRegistrationCreateCommand;
 import ch.epfl.biop.registration.scijava.command.PairRegistrationSift2DAffineCommand;
+import loci.common.DebugTools;
 import net.imagej.ImageJ;
 
 public class DemoRegistrationWorkflow {
@@ -10,6 +11,7 @@ public class DemoRegistrationWorkflow {
     static final ImageJ ij = new ImageJ();
 
     static public void main(String... args) throws Exception {
+        DebugTools.setRootLevel("INFO");
         ij.ui().showUI();
         /*Thread.sleep(5000);
         ij.command().run(CreateBdvDatasetQuPathCommand.class,true).get();
